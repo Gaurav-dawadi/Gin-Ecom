0bfea7f2ef5506f152bbe5449daa98675e7d7e0b
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-05-26 16:34:05 +0545

    fix: field name in Comment model represententing product model

--- models/comment.go
+++ models/comment.go
@@ -4,6 +4,6 @@ type Comment struct {
 	Base
 	// Parent sql.NullInt64 `json:"parent"`
 	UserId    uint   `gorm:"not null"`
-	ProductId uint   `gorm:"not null"`
+	ProductID uint   `gorm:"not null"`
 	Body      string `json:"body" gorm:"size:1024"`
 }
