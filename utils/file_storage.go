66976ff76ddaa2a964f2b98a853531479a35bb82
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-06-06 08:59:33 +0545

    refactor: generate random filename for a file

--- utils/file_storage.go
+++ utils/file_storage.go
@@ -6,11 +6,30 @@ import (
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 )
 
+// Generate Random file name for a file
+func generate_filename(filename string) string {
+	fileExtension := filepath.Ext(filename)
+	random_filename := strconv.FormatInt(time.Now().UnixNano(), 10) + fileExtension
+	return random_filename
+}
+
 func FileSystemStorage(file multipart.File, uploadFile *multipart.FileHeader) string {
-	filename := uploadFile.Filename
-	out, err := os.Create(filepath.Join("media", filepath.Base(filename)))
+	// Check if directory to store all files exists. If not, first create them.
+	dirInfo, err := os.Stat("media")
+	if os.IsNotExist(err) {
+		if err := os.Mkdir(dirInfo.Name(), 0777); err != nil {
+			fmt.Println("Error occured during creation of directory: ", err)
+		}
+	}
+
+	filename := filepath.Base(uploadFile.Filename)
+	new_filename := generate_filename(filename)
+
+	out, err := os.Create(filepath.Join(dirInfo.Name(), new_filename))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -20,6 +39,6 @@ func FileSystemStorage(file multipart.File, uploadFile *multipart.FileHeader) st
 		fmt.Println(err)
 	}
 
-	filepath := "media/" + filename
+	filepath := dirInfo.Name() + new_filename
 	return filepath
 }
