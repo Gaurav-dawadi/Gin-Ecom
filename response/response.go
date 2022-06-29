ffe7a534a0a1092e6cac33e18b3ae635c4ea6e88
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-06-29 12:11:48 +0545

    feat: create JWTError Response for showing error

--- response/response.go
+++ response/response.go
@@ -1,7 +1,10 @@
 package response
 
 import (
+	"go-practice/utils"
 	"net/http"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type ResponseMessage struct {
@@ -31,5 +34,29 @@ func ResponseBadRequest(message string) ResponseMessage {
 		Message: message,
 		Status:  http.StatusBadRequest,
 		Code:    "bad_request",
+	}
+}
+
+func JWTErrorResponse(err error) ResponseMessage {
+	var code string
+	var status int
+
+	code = utils.BAD_REQUEST
+	status = http.StatusBadRequest
+
+	if err == jwt.ErrSignatureInvalid {
+		status = http.StatusBadRequest
+		code = utils.BAD_REQUEST
+	}
+
+	if err == jwt.ErrTokenExpired {
+		status = http.StatusUnauthorized
+		code = utils.TOKEN_EXPIRED
+	}
+
+	return ResponseMessage{
+		Message: err.Error(),
+		Status:  status,
+		Code:    code,
 	}
 }
