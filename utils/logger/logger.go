4849fab53b34d9670df39c798a2496bae7ee7a16
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-07-11 14:00:55 +0545

    feat: create sub-package logger inside util package

---
+++ utils/logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"log"
+
+	"github.com/logrusorgru/aurora"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("Fatal\n%s: %s", aurora.Red(msg), aurora.Red(err))
+	}
+}
+
+func LogError(err error, msg string) {
+	log.Printf("%s: %s", aurora.Red(msg), aurora.Red(err))
+}
+
+func LogOutput(msg string) {
+	log.Println(aurora.Cyan(msg))
+}
+
+func LogInfo(msg string) {
+	log.Println(aurora.Blue(msg))
+}
+
+// Log logs msg with default color.
+func Log(msg string) {
+	log.Println(msg)
+}
+
+// Success logs msg with green color.
+func Success(msg string) {
+	log.Println(aurora.Green(msg))
+}
+
+// Info logs msg with blue color.
+func Info(msg string) {
+	log.Println(aurora.Cyan(msg))
+}
+
+// Error logs msg with Red color.
+func Error(err error, msg string) {
+	log.Printf("%s: %s", aurora.Red(msg), aurora.Red(err))
+}
