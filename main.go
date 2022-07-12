622e94a4f8eaae70060cc562b674074bf918c3c3
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-07-12 17:14:36 +0545

    refactor: change fmt to logger in main.go

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"go-practice/application"
-	"log"
+	"go-practice/utils/logger"
 
 	"github.com/joho/godotenv"
 )
 
 func loadEnv() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Couldnot load environment file")
+		logger.LogFatal("Couldnot load environment file")
 	}
 }
 
 func main() {
-	fmt.Println("Before loading env")
+	logger.LogInfo("Before loading env")
 	loadEnv()
-	fmt.Println("After loading env")
+	logger.LogInfo("After loading env")
 	application.ApplicationInitialize.RunApplication(application.ApplicationInitialize{})
 }
